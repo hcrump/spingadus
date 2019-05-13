347662d386460f2b481908ddfc8cac194cf37957
Horace Crump <hcrump@gmail.com>
2019-05-12 23:32:32 -0700

    working scroll and cursor

--- static/scripts/static-screen.js
+++ static/scripts/static-screen.js
@@ -24,26 +24,65 @@ function testwriter(xhttp){
 }
 
 function delayme(arr,speed){
-    console.log(arr);
-    arr.forEach(function(el,i){
-        console.log(arr.length);
-        setTimeout(function(){
-            document.getElementById('scrollTextPre').innerHTML += arr[i];
-            if(i = el.length-1){
-                console.log('br');
-                document.getElementById('scrollTextPre').innerHTML += '<br>';
+    console.log('delayme()');
+    var aText = new Array(
+    "There ",
+    "Those","what","asdf", "fuckk","noway","buh","bb","cc"
+    );
+    aText = arr;
+    console.log(aText);
+    var iSpeed = speed; // time delay of print out
+    var iIndex = 0; // start printing array at this posision
+    var iArrLength = aText[0].length; // the length of the text array
+    var iScrollAt = 20; // start scrolling up at this many lines
+
+    var iTextPos = 0; // initialise text position
+    var sContents = ''; // initialise contents variable
+    var iRow; // initialise current row
+
+    var typewriter2 = function() {
+        sContents =  ' ';
+        iRow = Math.max(0, iIndex-iScrollAt);
+        var destination = document.getElementById("scrollTextDiv");
+        while ( iRow < iIndex ) {
+            sContents += aText[iRow++] + '<br />';
+        }
+        destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+        if ( iTextPos++ == iArrLength ) {
+            iTextPos = 0;
+            iIndex++;
+            if ( iIndex != aText.length ) {
+                iArrLength = aText[iIndex].length;
+                setTimeout(typewriter2, iSpeed);
             }
-        },(speed*i));
-        document.getElementById('scrollTextPre').innerHTML += '<br>';
-    })
+        } else {
+            setTimeout(typewriter2, iSpeed);
+        }
+    };
+    typewriter2();
 }
+
+// function delayme(arr,speed){
+//     //console.log(arr);
+//     arr.forEach(function(el,i){
+//         console.log(arr.length);
+//         setTimeout(function(){
+//             document.getElementById('scrollTextPre').innerHTML += arr[i];
+//             if(i = el.length-1){
+//                 console.log('br');
+//                 document.getElementById('scrollTextPre').innerHTML += '<br>';
+//             }
+//         },(speed*i));
+//     })
+// }
 
 function typeWriter(xhttp) {
     console.log('typeWriter()');
     var txt = xhttp;
     var lines = txt.split(/\r\n|\n|\r/);
+    lines = fixArray(lines);
     //console.log(lines);
-    var speed = 200; //ms
+    var speed = 50; //ms
     // var lines = ['aaaaaa','bbbbbb','cccwcccc'];
     delayme(lines,speed);
 }
@@ -61,6 +100,13 @@ function loadDoc(filename,callback) {
     httpRequest.send();
 }
 
+function fixArray(arr){
+    let tmp = arr;
+    while(tmp[0] == ''){
+        tmp.shift();
+    }
+    return tmp;
+}
 var number = 0;
 var toggle = 0;
 //document.body.className = 'myColor';
